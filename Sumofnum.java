890124066f606cb0e0157041d6110fdda3647b34
mahaginfotech <mahaginfotech@gmail.com>
2017-06-06 00:08:55 +0530

    Create Sumofnum.java

---
+++ Sumofnum.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.Scanner;
+class Sumofnum
+{
+public static void main(String args[])
+{
+int n,sum=0;
+Scanner sc=new Scanner(system.in);
+for(int i=0;i<n;i++)
+{
+sum=sum+i;
+}
+System.out.println(sum);
+}
+}
