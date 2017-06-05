62a91e4bc22c7df99d576ec13991df0c5d022512
mahaginfotech <mahaginfotech@gmail.com>
2017-06-05 22:05:15 +0530

    Create Natural.java

---
+++ Natural.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Natural
+{
+public static void main(String args[])
+{
+int n,i=1;
+int sum=0;
+while(i<=n)
+{
+sum=sum+i;
+}
+System.out.println("Natural num is"+sum);
+}
+}
