7b7504133450e16995c5c3e1a8e4d6794e61a982
mahaginfotech <mahaginfotech@gmail.com>
2017-06-06 00:37:54 +0530

    Create Power.java

---
+++ Power.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Power
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+Long num=sc.nextLong();
+Long power=sc.nextLong();
+if(num>=0 && power==0)
+{
+res=1;
+}
+else if(num==0 && power>=0)
+{
+res=0;
+}
+else
+for(int i=0;i<p;i++)
+{
+res=res*num;
+}}
+}
+
