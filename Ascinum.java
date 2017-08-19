61f339be44cf52792ef5229f4bca96803a322857
mahaginfotech <mahaginfotech@gmail.com>
2017-08-19 10:25:08 +0530

    Create Ascinum.java

---
+++ Ascinum.java
@@ -0,0 +1,15 @@
+public class Ascinum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+
+		
+		int num=1;
+		while(num>0 && num<255)
+		{
+			System.out.println(num+" : "+(char)num);
+			num++;
+		}
+	}
+
+}
